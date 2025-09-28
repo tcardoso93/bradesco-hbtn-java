83f32c268193a918cf1e53b85a56b255d46f0a70
Tiago Oliveira <tcaroliveira@gmail.com>
2025-09-28 20:25:23 -0300

    Implementação interfaces_exercicios resumo pedido

---
+++ Interfaces_Exercicios/resumo_pedido/ProcessadorPedido.java
@@ -0,0 +1,16 @@
+import provedores.Frete;
+import provedores.ProvedorFrete;
+
+public class ProcessadorPedido {
+    private ProvedorFrete provedorFrete;
+
+    public ProcessadorPedido(ProvedorFrete provedorFrete) {
+        this.provedorFrete = provedorFrete;
+    }
+
+    public void processar(Pedido pedido) {
+        Frete frete = new Frete();
+        frete = provedorFrete.calcularFrete(pedido.getPeso(), pedido.getTotal());
+        pedido.setFrete(frete);
+    }
+}
